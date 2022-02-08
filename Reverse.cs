e1c95e336c0d9e6b187fe715ea0da3ae112c861b
pradeep2329 <99229235+pradeep2329@users.noreply.github.com>
2022-02-08 10:13:02 +0530

    Add files via upload

---
+++ Reverse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Assignment
+{
+    class Reversebywords
+    {
+        static void Main()
+        {
+            reversebywords();
+        }
+        static void reversebywords()
+        {
+            Console.WriteLine("Enter  Sentence:");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(' ');
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.Write(words[i] + "   ");
+            }
+        }
+    }
+}
