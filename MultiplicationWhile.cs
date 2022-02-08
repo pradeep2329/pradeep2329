ce9ecc1906d2eceed75cee40026c401080719de8
pradeep2329 <99229235+pradeep2329@users.noreply.github.com>
2022-02-08 10:19:24 +0530

    Add files via upload

---
+++ MultiplicationWhile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2while
+{
+    public class multiplicationWhile
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number  :");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int i = 1; //initialization
+            while (i <= 10) //condition
+            {
+                Console.WriteLine(a + "*" + i + "=" + (a * i));
+                i++; //increament
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
