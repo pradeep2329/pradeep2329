797638772c0e4f8661869ed5b0501c46fee6cc45
pradeep2329 <99229235+pradeep2329@users.noreply.github.com>
2022-02-08 10:23:25 +0530

    Add files via upload

---
+++ validateLeafyear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4validdate
+{
+    class leafyear
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (((year%4==0)&&(year%100 != 0) || (year%400==0)))
+            {
+                Console.WriteLine("{0} is a leap year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leaf year", year);
+            }
+        }
+    }
+}
