f9cd5d6e35407fdce4fc99f68b4413031d0046b9
pradeep2329 <99229235+pradeep2329@users.noreply.github.com>
2022-02-08 10:21:30 +0530

    Add files via upload

---
+++ PostiveandNegative.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3posandnegIfclause
+{
+    public class postiveandnegtive
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Enter the number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a>=0)
+            {
+                Console.WriteLine("{0} is Positive number", a);
+
+            }
+            else
+            {
+                Console.WriteLine("{0} is a negative number",a);
+            }
+        }
+    }
+}
