7c337b6d6f3456ca557043757ff40509dd5c6dda
pradeep2329 <99229235+pradeep2329@users.noreply.github.com>
2022-02-08 10:55:09 +0530

    Add files via upload

---
+++ Cylinder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleCylinder
+{
+    class CircleAndCylinder
+    {
+        static void Main(string[] args)
+        {
+
+            Cylinder cylinders = new Cylinder(12.34, 10.0, "Black");
+
+            Cylinder[] circles = {
+                                    new Cylinder(12.34),
+                                    new Cylinder(12.34, 10.0),
+                                    new Cylinder(12.34, 10.0, "Black")
+
+                                };
+            Console.WriteLine(circles[0].getVolume());
+            Console.WriteLine(circles[1].getVolume());
+            Console.WriteLine(circles[2].getVolume());
+
+            Console.ReadLine();
+
+        }
+    }
+    public class Circle
+    {
+        public double radius;
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        public double getArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    public class Cylinder
+    {
+        public Circle dt;
+        public double height;
+        public string color;
+
+        public Cylinder(double r, double h, string c)
+        {
+            dt = new Circle(r);
+            height = h;
+        }
+        public Cylinder(double r, double h)
+        {
+            dt = new Circle(r);
+            height = h;
+        }
+        public Cylinder(double r)
+        {
+            dt = new Circle(r);
+
+        }
+        public double getVolume()
+        {
+            return dt.getArea() * height;
+
+        }
+    }
+}
