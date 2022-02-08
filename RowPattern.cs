6bfaa9dc40a420440f3a40cf1d97fb48984ec65f
pradeep2329 <99229235+pradeep2329@users.noreply.github.com>
2022-02-08 07:34:39 +0530

    Add files via upload

---
+++ RowPattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    class StarPattern
+    {
+        public static void Main()
+        {
+            int i;
+            int j; 
+            int rows;
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+                
+        }
+    }
+}
