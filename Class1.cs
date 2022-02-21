0850e4692ddd023cff158ac73b01737175f1bde1
pradeep2329 <99229235+pradeep2329@users.noreply.github.com>
2022-02-21 13:53:49 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductClassLibrary
+{
+    public class Product
+    {
+
+        public int DressCode { get; set; }
+        public string DressName { get; set; }
+        public string Brand { get; set; }
+        public string Category  { get; set; }
+        public string DressSize{ get; set; }
+        public decimal Price { get; set; }
+    }
+}
